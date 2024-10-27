d0ea46d097e5c9d8947f2d11f310efeafb014e5a
adel <adelramdani27@gmail.com>
2024-10-27 20:22:59 +0100

    did the middleware

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const path = require('path');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use((req,res,next) => {
+    const date = new Date
+    
+    if(date.getHours() < 9 || date.getHours() > 17){
+        console.log('The website is closed come back later')
+        res.send('The website is closed come back later.')
+    }
+    else { 
+        next()
+    }
+})
 
 app.use(express.static(path.join(__dirname, 'pages')));
 
@@ -15,6 +25,10 @@ app.get('/', (req, res) => {
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'contact.html'));
 });
+app.post('/contact' , (req,res) => {
+    console.log(req.body);
+    res.redirect('/')
+})
 
 app.get('/service', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'service.html'));
